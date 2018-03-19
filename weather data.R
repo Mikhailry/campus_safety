cce9bdab2bfb39dcc660c4e0fb06a5f3703f09e3
parkerjoncus <parkerj@me.com>
2018-03-18 19:24:23 -0500

    student enrollment values and weather data start code
    The student enrollment numbers are in an excel sheet and the code will
    read it into a data frame.
    The weather data code is not finished yet, it is currently just how to
    obtain the weather for a range, the key is my key and it can only be
    used 500 times a day, 10 times a minute.

---
+++ weather data.R
@@ -0,0 +1,9 @@
+#https://cran.r-project.org/web/packages/rwunderground/README.html
+install.packages('rwunderground')
+library(rwunderground)
+
+rwunderground::set_api_key('651e75c69a9131c3')
+key=get_api_key()
+location=set_location(territory = 'Illinois', city = 'Chicago')
+#Warning, only 10 calls per minute which the function has a default parameter to slow down. Also only 500 calls per day (1 day of weather = 1 call)
+weather=data.frame(history_range(location = location, date_start = '20150101', date_end = '20150110'))
