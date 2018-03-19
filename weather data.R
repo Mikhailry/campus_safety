b1043349180fd7134ba370edb6ceac807f602fdc
parkerjoncus <parkerj@me.com>
2018-03-18 21:31:25 -0500

    weather 2 data added
    weather 2 data is from 4/27/2015 to 11/13/2015

--- weather data.R
+++ weather data.R
@@ -1,9 +1,10 @@
 #https://cran.r-project.org/web/packages/rwunderground/README.html
-install.packages('rwunderground')
+library(devtools)
+devtools::install_github('ALShum/rwunderground')
 library(rwunderground)
 
 rwunderground::set_api_key('651e75c69a9131c3')
 key=get_api_key()
 location=set_location(territory = 'Illinois', city = 'Chicago')
 #Warning, only 10 calls per minute which the function has a default parameter to slow down. Also only 500 calls per day (1 day of weather = 1 call)
-weather20140101_20150426=data.frame(history_range(location = location, date_start = '20140101', date_end = '20150426'))
+weather20150427_20151113=data.frame(history_range(location = location, date_start = '20150427', date_end = '20151113'))
