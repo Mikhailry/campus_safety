b22a555b1d071419d46b7f3b6bd5c93da6a29df8
Parker Joncus <parkerjoncus@dhcp4.merusouth125.iit.edu>
2018-03-29 16:59:52 -0500

    changed path
    changed path for student enroll

--- Parker Codes/studentenroll.R
+++ Parker Codes/studentenroll.R
@@ -1,6 +1,6 @@
 library(readxl)
 #Be sure to edit path
-IITenroll <- read_excel("~/Documents/Math 571 project/SchoolEnrollment.xlsx", sheet = "IIT enroll")
-UCIenroll <- read_excel("~/Documents/Math 571 project/SchoolEnrollment.xlsx", sheet = "Uchi enroll")
+IITenroll <- read_excel("~/Documents/Math 571 project/Data/SchoolEnrollment.xlsx", sheet = "IIT enroll")
+UCIenroll <- read_excel("~/Documents/Math 571 project/Data/SchoolEnrollment.xlsx", sheet = "Uchi enroll")
 View(IITenroll)
 View(UCIenroll)
