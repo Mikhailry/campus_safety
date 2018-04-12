09a1095b3c354a45a93b75edf9129e2ed9847e14
parkerjoncus <parkerj@me.com>
2018-04-12 15:19:13 -0500

    code updates

--- Parker Codes/Correlation_anlaysis.R
+++ Parker Codes/Correlation_anlaysis.R
@@ -16,4 +16,15 @@ plot(sort(table(as.factor(Uchicago_campus_crimes_cleaned$weather_cond_exact)), d
 plot(sort(table(as.factor(Crime_2014toPres_IIT_area$weather_cond_exact)), decreasing = TRUE)[1:8])
 plot(sort(table(as.factor(Crime_2014toPres_UC_area$weather_cond_exact)), decreasing = TRUE)[1:8])
 
+#Most common crimes 
+sort(table(factor(Crime_2014toPres_IIT_area$`Primary Type`)), decreasing = TRUE)[1:8]
+sort(table(factor(Crime_2014toPres_UC_area$`Primary Type`)), decreasing = TRUE)[1:8]
+plot(sort(table(factor(Crime_2014toPres_IIT_area$`Primary Type`)), decreasing = TRUE)[1:8])
+plot(sort(table(factor(Crime_2014toPres_UC_area$`Primary Type`)), decreasing = TRUE)[1:8])
+
 #contingency table
+t<-table(factor(Crime_2014toPres_IIT_area$weather_cond_exact),factor(Crime_2014toPres_IIT_area$`Primary Type`))
+table(factor(Crime_2014toPres_UC_area$weather_cond_exact),factor(Crime_2014toPres_UC_area$`Primary Type`))
+
+#algstat package
+
