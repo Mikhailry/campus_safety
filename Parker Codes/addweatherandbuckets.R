b6731df3be3e7bbfb5d031950ae99f054f89d206
parkerjoncus <parkerj@me.com>
2018-04-21 15:12:35 -0500

    add weather and bucket code
    code to add time buckets, month, day, exact condition, standard
    condition, severity, precipitation, temperature, and humidity

---
+++ Parker Codes/addweatherandbuckets.R
@@ -0,0 +1,66 @@
+library(lubridate)
+#read data
+IIT_FINAL_AGG <- read_csv("~/Documents/Math 571 project/IIT_FINAL_AGG.csv")
+UC_FINAL_AGG <- read_csv("~/Documents/Math 571 project/UC_FINAL_AGG.csv")
+
+#Change timezone and add so it is correct time
+attr(IIT_FINAL_AGG$OCCURED,"tzone")<-"America/Chicago"
+attr(UC_FINAL_AGG$OCCURED,"tzone")<-"America/Chicago"
+IIT_FINAL_AGG$OCCURED<-IIT_FINAL_AGG$OCCURED+hours(5)
+UC_FINAL_AGG$OCCURED<-UC_FINAL_AGG$OCCURED+hours(5)
+
+#time buckets, day of week, and month
+IIT_FINAL_AGG$MONTH<-NA
+UC_FINAL_AGG$MONTH<-NA
+IIT_FINAL_AGG$DAY<-NA
+UC_FINAL_AGG$DAY<-NA
+IIT_FINAL_AGG$TIME_BUCKET<-NA
+UC_FINAL_AGG$TIME_BUCKET<-NA
+
+IIT_FINAL_AGG$MONTH<-as.numeric(strftime(IIT_FINAL_AGG$OCCURED, format= '%m', tz='America/Chicago'))
+UC_FINAL_AGG$MONTH<-as.numeric(strftime(UC_FINAL_AGG$OCCURED, format= '%m', tz='America/Chicago'))
+IIT_FINAL_AGG$DAY<-weekdays(IIT_FINAL_AGG$OCCURED)
+UC_FINAL_AGG$DAY<-weekdays(UC_FINAL_AGG$OCCURED)
+#0:0-3   1:3-6    2:6-9   3:9-12   4:12-15    5:15-18   6:18-21   7:21-24
+IIT_FINAL_AGG$TIME_BUCKET<-floor((as.numeric(strftime(IIT_FINAL_AGG$OCCURED, format= '%H', tz='America/Chicago')))/3)
+UC_FINAL_AGG$TIME_BUCKET<-floor((as.numeric(strftime(UC_FINAL_AGG$OCCURED, format= '%H', tz='America/Chicago')))/3)
+
+#Add weather based on the times for IIT and UC
+load("~/Documents/Math 571 project/Data/cleanweatherFull.Rda")
+IIT_FINAL_AGG$COND<-NA
+IIT_FINAL_AGG$STAND_COND<-NA
+IIT_FINAL_AGG$SEVERITY<-NA
+IIT_FINAL_AGG$TEMP<-NA
+IIT_FINAL_AGG$HUM<-NA
+IIT_FINAL_AGG$WIND<-NA
+IIT_FINAL_AGG$PRECIP<-NA
+
+UC_FINAL_AGG$COND<-NA
+UC_FINAL_AGG$STAND_COND<-NA
+UC_FINAL_AGG$SEVERITY<-NA
+UC_FINAL_AGG$TEMP<-NA
+UC_FINAL_AGG$HUM<-NA
+UC_FINAL_AGG$WIND<-NA
+UC_FINAL_AGG$PRECIP<-NA
+
+for (i in 1:length(IIT_FINAL_AGG$OCCURED)){
+  min_diff<-min(abs(IIT_FINAL_AGG$OCCURED[i]-weatherFull$date))
+  row<-as.numeric(row.names(weatherFull[abs(IIT_FINAL_AGG$OCCURED[i]-weatherFull$date)==min_diff,]))
+  IIT_FINAL_AGG$STAND_COND[i]<-weatherFull$condition[row][1]
+  IIT_FINAL_AGG$SEVERITY[i]<-weatherFull$severity[row][1]
+  IIT_FINAL_AGG$TEMP[i]<-weatherFull$temp[row][1]
+  IIT_FINAL_AGG$COND[i]<-weatherFull$cond[row][1]
+  IIT_FINAL_AGG$WIND[i]<-weatherFull$wind_spd[row][1]
+  IIT_FINAL_AGG$HUM[i]<-weatherFull$hum[row][1]
+  IIT_FINAL_AGG$PRECIP[i]<-weatherFull$precip[row][1]
+}
+
+for (i in 1:length(Crime_2014toPres_IIT_area$Date)){
+  min_diff<-min(abs(Crime_2014toPres_IIT_area$Date[i]-weatherFull$date))
+  row<-as.numeric(row.names(weatherFull[abs(Crime_2014toPres_IIT_area$Date[i]-weatherFull$date)==min_diff,]))
+  Crime_2014toPres_IIT_area$weather_cond[i]<-weatherFull$condition[row][1]
+  Crime_2014toPres_IIT_area$weather_sev[i]<-weatherFull$severity[row][1]
+  Crime_2014toPres_IIT_area$temperature[i]<-weatherFull$temp[row][1]
+  Crime_2014toPres_IIT_area$weather_cond_exact[i]<-weatherFull$cond[row][1]
+}
+
