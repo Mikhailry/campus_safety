f3b3be03d9ae5d05602baf04538334aa123ebc12
Parker Joncus <parkerjoncus@Parkers-MacBook-Pro.local>
2018-04-21 20:17:19 -0500

    missed some weather descriptions
    now less NAs for Stand_Cond

--- Parker Codes/addweatherandbuckets.R
+++ Parker Codes/addweatherandbuckets.R
@@ -74,6 +74,43 @@ end<-Sys.time()
 IIT_FINAL_AGG$PRECIP[is.na(IIT_FINAL_AGG$PRECIP)]<-0.00
 UC_FINAL_AGG$PRECIP[is.na(IIT_FINAL_AGG$PRECIP)]<-0.00
 
+#missed a couple of weather descriptions
+for (i in 1:length(IIT_FINAL_AGG$OCCURED)){
+  if(is.na(IIT_FINAL_AGG$STAND_COND[i])){
+    node<-IIT_FINAL_AGG$COND[i]
+    if (!is.na(node)){
+      if(node == 'Thunderstorms and Rain'){
+        IIT_FINAL_AGG$STAND_COND[i]<-('Thunderstorms')
+        IIT_FINAL_AGG$SEVERITY[i]<-('Medium')
+      }
+      else if (node == 'Patches of Fog'){
+        IIT_FINAL_AGG$STAND_COND[i]<-('Fog')
+        IIT_FINAL_AGG$SEVERITY[i]<-('Low')
+      }
+    }
+  }
+}
+
+for (i in 1:length(UC_FINAL_AGG$OCCURED)){
+  if(is.na(UC_FINAL_AGG$STAND_COND[i])){
+    node<-UC_FINAL_AGG$COND[i]
+    if (!is.na(node)){
+      if(node == 'Thunderstorms and Rain'){
+        UC_FINAL_AGG$STAND_COND[i]<-('Thunderstorms')
+        UC_FINAL_AGG$SEVERITY[i]<-('Medium')
+      }
+      else if (node == 'Patches of Fog'){
+        UC_FINAL_AGG$STAND_COND[i]<-('Fog')
+        UC_FINAL_AGG$SEVERITY[i]<-('Low')
+      }
+      else if (node == 'Thunderstorms with Small Hail'){
+        UC_FINAL_AGG$STAND_COND[i]<-('Thunderstorms')
+        UC_FINAL_AGG$SEVERITY[i]<-('Low')
+      }
+    }
+  }
+}
+
 #Save data
 setwd("~/Documents/Math 571 project")
 save(IIT_FINAL_AGG, file = 'IIT_FINAL_AGG.Rda')
