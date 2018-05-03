f2ac176243a16a0e9b6d2f93970e7061dc3c1da2
Parker Joncus <parkerjoncus@dhcp66.merunorth109.iit.edu>
2018-05-03 09:42:51 -0500

    added comments

--- Parker Codes/weather data.R
+++ Parker Codes/weather data.R
@@ -9,3 +9,9 @@ key=get_api_key()
 location=set_location(territory = 'Illinois', city = 'Chicago')
 #Warning, only 10 calls per minute which the function has a default parameter to slow down. Also only 500 calls per day (1 day of weather = 1 call)
 weather20170329_20180319=data.frame(history_range(location = location, date_start = '20170329', date_end = '20180319'))
+
+#Dates needed to be inputed manually so that we did not go over the call limit in a day.
+#first date range used: 1/1/2014-4/26/2015
+#second date range used: 4/27/2015-11/13/2015
+#third date range used: 11/14/2015-3/28/2017
+#fourth data range used: 3/29/2017-3/19/2018
