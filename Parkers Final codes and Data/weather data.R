e19ba502f73c95aa571b0a9627a3a9c07f9e9f0b
Parker Joncus <parkerjoncus@dhcp66.merunorth109.iit.edu>
2018-05-03 10:18:24 -0500

    code cleaning and commenting

--- Parkers Final codes and Data/weather data.R
+++ Parkers Final codes and Data/weather data.R
@@ -1,12 +1,14 @@
 #https://cran.r-project.org/web/packages/rwunderground/README.html
-vcxvc#https://cran.r-project.org/web/packages/rwunderground/README.html
+#install rwunderground package
 library(devtools)
 devtools::install_github('ALShum/rwunderground')
 library(rwunderground)
 
+#set api key and location
 rwunderground::set_api_key('651e75c69a9131c3')
 key=get_api_key()
 location=set_location(territory = 'Illinois', city = 'Chicago')
+
 #Warning, only 10 calls per minute which the function has a default parameter to slow down. Also only 500 calls per day (1 day of weather = 1 call)
 #weather20140101_20150426=data.frame(history_range(location = location, date_start = '20140101', date_end = '20150426'))
 #weather20150427_20151113=data.frame(history_range(location = location, date_start = '20150427', date_end = '20151113'))
@@ -19,19 +21,20 @@ weather20170329_20180319=data.frame(history_range(location = location, date_star
 #third date range used: 11/14/2015-3/28/2017
 #fourth data range used: 3/29/2017-3/19/2018
 
+#set path for saving and loading
 path<-"~/Documents/Math 571 project/Parkers Final codes and Data"
 setwd(path)
   
-#save(weather20140101_20150426, file = 'weather1.rda')
-#save(weather20150427_20151113, file = 'weather2.rda')
-#save(weather20151114_20170328, file = 'weather3.rda')
-#save(weather20170329_20180319, file = 'weather4.rda')
+#save(weather20140101_20150426, file = 'weather1.Rda')
+#save(weather20150427_20151113, file = 'weather2.Rda')
+#save(weather20151114_20170328, file = 'weather3.Rda')
+#save(weather20170329_20180319, file = 'weather4.Rda')
 
-#load(weather1.rda)
-#load(weather2.rda)
-#load(weather3.rda)
-#load(weather4.rda)
+load('weather1.Rda')
+load('weather2.Rda')
+load('weather3.Rda')
+load('weather4.Rda')
 
-#Merge data
+#Merge weather data to get full data set and save.
 weatherFull<-rbind(weather20140101_20150426, weather20150427_20151113, weather20151114_20170328, eather20170329_20180319)
-
+save(weatherFull, file = 'weatherFull.Rda')
